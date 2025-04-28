6c5a509697d9376aee62c2e4649125570611f875
Nawfal <134062420+xFalzz@users.noreply.github.com>
2025-04-28 19:43:38 +0700

    Update TextGenerate.tsx

--- app/_components/ui/TextGenerate.tsx
+++ app/_components/ui/TextGenerate.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useEffect } from "react";
 import { motion, stagger, useAnimate } from "framer-motion";
 import { cn } from "@/app/_lib/utils";
@@ -8,27 +9,34 @@ export const TextGenerateEffect = ({
   className,
   filter = true,
   duration = 0.5,
+  delayStart = 0, // TAMBAHKAN prop untuk delay start
 }: {
   words: string;
   className?: string;
   filter?: boolean;
   duration?: number;
+  delayStart?: number; // delay dalam ms
 }) => {
   const [scope, animate] = useAnimate();
   let wordsArray = words.split(" ");
+
   useEffect(() => {
-    animate(
-      "span",
-      {
-        opacity: 1,
-        filter: filter ? "blur(0px)" : "none",
-      },
-      {
-        duration: duration ? duration : 1,
-        delay: stagger(0.2),
-      }
-    );
-  }, [scope.current]);
+    const timeoutId = setTimeout(() => {
+      animate(
+        "span",
+        {
+          opacity: 1,
+          filter: filter ? "blur(0px)" : "none",
+        },
+        {
+          duration: duration ? duration : 1,
+          delay: stagger(0.2),
+        }
+      );
+    }, delayStart);
+
+    return () => clearTimeout(timeoutId);
+  }, [scope.current, animate, filter, duration, delayStart]);
 
   const renderWords = () => {
     return (
